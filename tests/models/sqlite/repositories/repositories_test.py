6f09389465f89fdf225e4c5785effe9bf5295680
GabrielHenriqueHE <gabriel.evaristo.2004@gmail.com>
2025-04-16 09:13:30 -0300

    feat: implementing People repository

--- tests/models/sqlite/repositories/repositories_test.py
+++ tests/models/sqlite/repositories/repositories_test.py
@@ -2,6 +2,7 @@ import pytest
 from mock_alchemy.mocking import UnifiedAlchemyMagicMock
 
 from src.models.sqlite.entities.pet import PetsTable
+from src.models.sqlite.repositories.people_repository import PeopleRepository
 from src.models.sqlite.repositories.pet_repository import PetRepository
 from src.models.sqlite.settings.connection import DBConnectionHandler
 
@@ -31,3 +32,16 @@ def test_delete_pet():
     print(pets_after_delete)
 
     assert len(pets_after_delete) == len(pets_before_delete) - 1 
+
+@pytest.mark.skip(reason='Teste de integração com o banco de dados.')
+def test_get_person():
+    connection_handler = DBConnectionHandler()
+    connection_handler.connect()
+
+    person_id: int = 1
+
+    repo = PeopleRepository(connection_handler)
+
+    response = repo.get_person(person_id)
+
+    print(response)
